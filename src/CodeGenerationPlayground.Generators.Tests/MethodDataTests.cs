121c9b0a8cfa53fc18be4cce5bae18b8e17d4766
Maikel Bos <maikel.bos0@gmail.com>
2025-03-02 16:16:38 +0100

    Add some tests for the data structs

---
+++ src/CodeGenerationPlayground.Generators.Tests/MethodDataTests.cs
@@ -0,0 +1,13 @@
+﻿using Xunit;
+
+namespace CodeGenerationPlayground.Generators.Tests;
+
+public class MethodDataTests {
+    [Fact]
+    public void EqualsByValue() {
+        var subject1 = new MethodData([], [], "Name");
+        var subject2 = new MethodData([], [], "Name");
+
+        Assert.True(subject1.Equals(subject2));
+    }
+}
---
+++ src/CodeGenerationPlayground.Generators.Tests/MethodOwnerDataTests.cs
@@ -0,0 +1,18 @@
+﻿using Xunit;
+
+namespace CodeGenerationPlayground.Generators.Tests;
+
+public class MethodOwnerDataTests {
+    [Theory]
+    [InlineData("Name", MethodOwnerType.Namespace, "Name", MethodOwnerType.Namespace, true)]
+    [InlineData("Name", MethodOwnerType.Class, "Name", MethodOwnerType.Class, true)]
+    [InlineData("Name", MethodOwnerType.Struct, "Name", MethodOwnerType.Struct, true)]
+    [InlineData("Name", MethodOwnerType.Class, "Other", MethodOwnerType.Class, true)]
+    [InlineData("Name", MethodOwnerType.Class, "Name", MethodOwnerType.Struct, false)]
+    public void EqualsByValue(string name1, MethodOwnerType type1, string name2, MethodOwnerType type2, bool expectedEquals) {
+        var subject1 = new MethodOwnerData(name1, type1);
+        var subject2 = new MethodOwnerData(name2, type2);
+
+        Assert.Equal(expectedEquals, subject1.Equals(subject2));
+    }
+}
