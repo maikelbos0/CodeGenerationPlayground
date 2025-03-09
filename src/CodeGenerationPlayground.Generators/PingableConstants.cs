a25025fa0d0ded19a674030feccf579296737f62
Maikel Bos <maikel.bos0@gmail.com>
2025-03-09 16:36:57 +0100

    Use nameof to resolve namespace

--- src/CodeGenerationPlayground.Generators/PingableConstants.cs
+++ src/CodeGenerationPlayground.Generators/PingableConstants.cs
@@ -1,6 +1,6 @@
 ﻿namespace CodeGenerationPlayground.Generators;
 
 public static class PingableConstants {
-    public const string FullyQualifiedAttributeName = "CodeGenerationPlayground.PingableAttribute";
+    public const string FullyQualifiedAttributeName = $"{nameof(CodeGenerationPlayground)}.PingableAttribute";
     public const string GlobalFullyQualifiedAttributeName = $"global::{FullyQualifiedAttributeName}";
 }
