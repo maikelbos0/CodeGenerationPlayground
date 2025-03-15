41a324e266a167603ff7e81fd332fb5db2683b95
Maikel Bos <maikel.bos0@gmail.com>
2025-03-15 07:28:10 +0100

    Start moving logic to a separate service for reuse and testability

---
+++ src/CodeGenerationPlayground.Generators/ValidatorMethod/ValidatorMethodService.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace CodeGenerationPlayground.Generators.ValidatorMethod;
+
+public class ValidatorMethodService {
+    private readonly PropertyDeclarationSyntax? propertyDeclarationSyntax;
+
+    public bool IsProperty => propertyDeclarationSyntax != null;
+
+    public ValidatorMethodService(/*SemanticModel semanticModel, */SyntaxNode node) {
+        propertyDeclarationSyntax = node as PropertyDeclarationSyntax;
+    }
+}
